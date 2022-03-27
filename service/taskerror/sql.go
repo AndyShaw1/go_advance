39d9be6849619d51e07a3525fba7e82ce02485c8
xws <xiaowenshuai@wepie.com>
2022-03-27 12:12:21 +0800

    feat(): 修改文案

--- service/taskerror/sql.go
+++ service/taskerror/sql.go
@@ -6,9 +6,6 @@ import (
 	"go_advance/store/db"
 )
 
-//我们在数据库操作的时候，比如 dao 层中当遇到一个 sql.ErrNoRows 的时候，是否应该 Wrap 这个 error，抛给上层。为什么，应该怎么做请写出代码？
-//应该抛出wrap后的error，这样会携带stack信息，方法定位问题，封装一个util方法，利用方法来替代err == sql.ErrNoRows的判断即可。
-
 type sqlStruct struct {
 }
 
